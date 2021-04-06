a40e6519025385693cc34fac36bc743d329d58c0
Etienne van Halteren <etiennevanhalteren@hotmail.com>
2021-04-06 14:09:57 +0200

    Add files via upload
    just the form

---
+++ guesGame.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Raad het nummer</title>
+    </head>
+    <body>
+        <h1>Raad het nummer</h1>
+        <form method="post">
+            <label for="gokken">Het getal is tussen 1 en 120</label>
+            <input type="number" height="2em" id="gokken" min="1" max="120" name="gokje">
+            <input type="submit">
+        </form>
+        
+    </body>
+</html>
