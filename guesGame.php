c7faa22cb1e88fd45c168e969482089945066241
daanminderman <daanminderman@gmail.com>
2021-04-06 15:53:10 +0200

    Functionaliteit van guessTheGame

--- guesGame.php
+++ guesGame.php
@@ -7,9 +7,25 @@
         <form method="post">
             <label for="gokken">Het getal is tussen 1 en 120</label>
             <input type="number" height="2em" id="gokken" min="1" max="120" name="gokje">
-            <input type="hidden" value="<?php // echo $hetNummer; ?>" name="hetNummer">
-            <input type="hidden" value="<?php // echo $counter; ?>" name="counter">
-            <input type="hidden" value="<?php // echo $average; ?>" name="average">
+            <input type="hidden" value="<?php 
+            if (isset($_POST["hetNummer"])) {
+                echo $_POST["hetNummer"];                
+            } else {
+                echo rand(1, 120);  
+            }
+            
+            ?>" name="hetNummer">
+            <input type="hidden" value="<?php  
+                if (isset($_POST["counter"])) {
+                    $counter = $_POST["counter"];
+                    $counter++;
+                    echo $counter;
+                } else {
+                    echo 0;
+                }
+                
+            ?>" name="counter">
+            <input type="hidden" value="<?php //echo $average; ?>" name="average">
             <?php  
                 /*   
                     loop maken voor eerdere pogingen?
@@ -20,6 +36,23 @@
             ?>
             <input type="submit">
         </form>
-        
+        <?php
+            if (isset($_POST["hetNummer"])) {
+                if ($_POST["hetNummer"] > $_POST["gokje"]) {
+                    echo "Guess higher<br>";
+                } elseif ($_POST["hetNummer"] < $_POST["gokje"]) {
+                    echo "Guess lower<br>";
+                } else {
+                    echo "Correct<br>";
+                }   if (isset($_POST["counter"]))
+                    echo $_POST["counter"];
+            }   
+        ?>
+        <form method="post" action="guesGame.php">
+            <input type="submit" value="Reset" name="reset">
+        </form>
+        <?php
+            $_POST["hetNummer"] = rand(1, 120);
+        ?>
     </body>
 </html>
